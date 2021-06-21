e93a560edd65f47121259e598152ec8d3d887239
Thiago M <sevilamare@gmail.com>
2021-06-21 20:18:17 -0300

    fonts

--- App.tsx
+++ App.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
 import SignIn from './src/screen/signin';
 
+import { useFonts } from 'expo-font';
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import {
+  Rajdhani_500Medium,
+  Rajdhani_700Bold,
+} from '@expo-google-fonts/rajdhani';
+import AppLoadling from 'expo-app-loading';
+
 const App = () => {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoadling />;
+  }
+
   return <SignIn />;
 };
 
