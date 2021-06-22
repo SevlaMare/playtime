40a7cfbfb5e7045256fb478b261c0f16a5cb88be
Thiago M <sevilamare@gmail.com>
2021-06-22 17:30:09 -0300

    svg config

---
+++ src/helpers/categories.ts
@@ -0,0 +1,8 @@
+import { ranked, duel, fun, training } from '../assets/icon';
+
+export const categories = [
+  { id: 1, title: 'Ranked', icon: ranked },
+  { id: 2, title: 'Duel 1v1', icon: duel },
+  { id: 3, title: 'Fun', icon: fun },
+  { id: 4, title: 'Pratice', icon: training },
+];
