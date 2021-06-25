90662d6ce03d3d1ac990d8b4bf95dc5e9c5502e1
Thiago M <sevilamare@gmail.com>
2021-06-25 20:47:59 -0300

    dotenv setup

--- src/config/discord.ts
+++ src/config/discord.ts
@@ -1,9 +1,9 @@
 const BASE_URL = 'https://discord.com/api';
-const REDIRECT_URI =
-  'https%3A%2F%2Fauth.expo.io%2F%40anonymous%2Fplaytime-b21415d0-e4bc-49f4-a43c-14db27d1d97e';
-const SCOPE = 'identify%20email%20connections%20guilds';
-const RESPONSE_TYPE = 'token';
-const CLIENT_ID = '857804257528381471';
-const CDN_IMAGE = 'https://cdn.discordapp.com';
+
+const { REDIRECT_URI } = process.env;
+const { SCOPE } = process.env;
+const { RESPONSE_TYPE } = process.env;
+const { CLIENT_ID } = process.env;
+const { CDN_IMAGE } = process.env;
 
 export { BASE_URL, REDIRECT_URI, SCOPE, RESPONSE_TYPE, CLIENT_ID, CDN_IMAGE };
