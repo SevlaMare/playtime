647a3121f848e6aff35e6b0533be70df966bc9c9
Thiago M <sevilamare@gmail.com>
2021-06-25 19:26:34 -0300

    access control

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,34 +1,40 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { useAuth } from './provider/auth';
 
 const { Navigator, Screen } = createStackNavigator();
 
-import Home from './screen/home';
 import SignIn from './screen/signin';
+import Home from './screen/home';
 import AppointmentCreate from './screen/appointmentCreate';
 import AppointmentDisplay from './screen/appointmentDisplay';
 import ModalGuild from './screen/modalGuild';
 
 import theme from './style/theme';
 
-// NavigationContainer wrap the navigation stack history
-// cardStyle bg, will be see on transitions in IOs
 const Router = () => {
+  const { user } = useAuth();
+
+  console.log('>>>>>>>', user.id);
+
   return (
     <NavigationContainer>
-      <Navigator
-        headerMode='none'
-        screenOptions={{
-          cardStyle: { backgroundColor: theme.background },
-        }}
-      >
-        <Screen name='SignIn' component={SignIn} />
-        <Screen name='Home' component={Home} />
-        <Screen name='AppointmentCreate' component={AppointmentCreate} />
-        <Screen name='AppointmentDisplay' component={AppointmentDisplay} />
-        <Screen name='ModalGuild' component={ModalGuild} />
-      </Navigator>
+      {user.id > 1 ? (
+        <Navigator
+          headerMode='none'
+          screenOptions={{
+            cardStyle: { backgroundColor: theme.background },
+          }}
+        >
+          <Screen name='Home' component={Home} />
+          <Screen name='AppointmentCreate' component={AppointmentCreate} />
+          <Screen name='AppointmentDisplay' component={AppointmentDisplay} />
+          <Screen name='ModalGuild' component={ModalGuild} />
+        </Navigator>
+      ) : (
+        <SignIn />
+      )}
     </NavigationContainer>
   );
 };
